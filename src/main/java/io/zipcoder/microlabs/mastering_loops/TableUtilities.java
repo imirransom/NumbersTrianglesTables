deb2aca8cfa9d5b1c11bd56da2a7a92f66b7f1da
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-31 07:31:09 -0400

    NumberUtilities done, working through Table/TriangleUtilites

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -10,6 +10,19 @@ public class TableUtilities {
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++){
+                sb.append(i*j);
+            } if (i < 10){
+                sb.append("\n  ");
+            } else if (i >= 10 && i <= 99){
+                sb.append("\n ");
+            } else {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,11 +3,22 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfRows; i++){
+            for (int j = 0; j < numberOfRows; j++){
+                sb.append("*\n");
+            }
+        } sb.append("*\n");
+        return sb.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfStars; i++){
+            sb.append("*");
+        }
+        return sb.toString();
     }
 
     public static String getSmallTriangle() {
