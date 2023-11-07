f24d9b7f64dbe8096449577d142e12c579aac62a
git-leon <xleonhunter@gmail.com>
2023-11-07 14:20:17 -0500

    simplified code

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -10,18 +10,10 @@ public class TableUtilities {
     }
 
     public static String getMultiplicationTable(int tableSize) {
-
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= tableSize; i++) {
             for (int j = 1; j <= tableSize; j++){
-             if ((i*j)< 10){
-                sb.append("  " + (i*j) +" |");
-            } else if ((i*j) <= 99){
-                sb.append(" " + (i*j) +" |");
-            } else {
-                sb.append((i*j) +" |");
-            }
-
+                sb.append(String.format("%3s", i*j)+" |");
             }
             sb.append("\n");
         }
